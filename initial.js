8d9a44a3ea2fc3fb3c943812d81b3c0ce7363ff0
Gabriel Guimarães <gabriel.souza.guimaraes01@gmail.com>
2021-12-08 10:05:43 -0300

    initial.js alterado.

--- initial.js
+++ initial.js
@@ -6,6 +6,11 @@ scene.background = new THREE.Color(0xddddff);
 const camera = new THREE.PerspectiveCamera(65, WIDTH/HEIGHT, 1, 800);
 camera.position.z = 4;
 
+const ambLight = new THREE.AmbientLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(2, 1, 2);
+scene.add(pointLight, ambLight);
+
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(WIDTH, HEIGHT);
 document.body.appendChild(renderer.domElement);
