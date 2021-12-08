e0988c44ddb6bd3fbcde1cb30190ced14afd207a
Gabriel Guimarães <gabriel.souza.guimaraes01@gmail.com>
2021-12-08 10:03:44 -0300

    main.js alterado.

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var numOfCubes = 20;
 
 for(let i = 0; i < numOfCubes; i++) {
-    let cubeCreator = new Cubes();
+    let cubeCreator = new Cubes(
+        randomNum(-2, 2),
+        randomNum(-2, 2),
+        randomNum(-2, 2)
+    );
     cubeCreator.build();
 }
 
